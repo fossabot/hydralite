1998ec44cd256b8f7e04c80d8a42c35af1586f30
firecraftgaming <eliyah@sundstroem.com>
2021-06-30 23:12:46 +0200

    feat(web): added project homepage

--- web/pages/project/[id]/[tab]/index.tsx
+++ web/pages/project/[id]/[tab]/index.tsx
@@ -1,4 +1,5 @@
-import styles from '../../../../styles/Project.module.scss'
+import styles from 'styles/Project.module.scss'
+import style from 'hoc/ProjectLayout/ProjectLayout.module.scss'
 import Meta from '~/components/Meta'
 import React from 'react'
 import { ProjectLayout } from 'hoc/ProjectLayout'
@@ -6,6 +7,7 @@ import { useRouter } from 'next/router'
 import { Pages } from 'hoc/ProjectLayout/pages'
 import { Case } from '~/components/Switch/Case'
 import { Switch } from '~/components/Switch/Switch'
+import { FlagIcon, GotoIcon, HornIcon, PersonIcon } from '~/components/Icons'
 
 const Project = () => {
     const router = useRouter();
@@ -19,7 +21,42 @@ const Project = () => {
         <div className={styles.project}>
             <ProjectLayout {...{selected, project}} >
                 <Switch value={selected}>
-                    <Case key="home">Home</Case>
+                    <Case key="home">
+                        <h2 className={style.homeTitle}>Welcome to <span style={{ color: '#695CFF' }}>{project}</span></h2>
+                        <h3 className={style.homeHeader}>Welcome to your shiny new project. Here’s some tips to get you setup.</h3>
+
+                        <div className={style.homeWidgetWrapper}>
+                            <div className={style.homeWidget}>
+                                <div className={style.homeWidgetIcon} style={{ backgroundColor: '#FFA2D4' }}><PersonIcon /></div>
+                                <div className={style.homeWidgetContent}>
+                                    <div className={style.homeWidgetTitle}>Invite your team</div>
+                                    <div className={style.homeWidgetText}>Get your teammates on board, and let the collaboration begin.</div>
+                                </div>
+                            </div>
+                            <div className={style.homeGoto}><GotoIcon /></div>
+                        </div>
+                        <div className={style.homeWidgetWrapper}>
+                            <div className={style.homeWidget}>
+                                <div className={style.homeWidgetIcon} style={{ backgroundColor: '#A2B1FF' }}><HornIcon /></div>
+                                <div className={style.homeWidgetContent}>
+                                    <div className={style.homeWidgetTitle}>Publicize your project</div>
+                                    <div className={style.homeWidgetText} style={{ width: 460 }}>Let others discover your project through hydra. Onboard potential contributors, all through one seamless platform.</div>
+                                </div>
+                            </div>
+                            <div className={style.homeGoto}><GotoIcon /></div>
+                        </div>
+                        <div className={style.homeWidgetWrapper}>
+                            <div className={style.homeWidget}>
+                                <div className={style.homeWidgetIcon} style={{ backgroundColor: '#FFA1A1' }}><FlagIcon /></div>
+                                <div className={style.homeWidgetContent}>
+                                    <div className={style.homeWidgetTitle}>Start developing</div>
+                                    <div className={style.homeWidgetText}>Go ahead and start building your project roadmap, View project traffic, and more</div>
+                                </div>
+                            </div>
+                            <div className={style.homeGoto}><GotoIcon /></div>
+                        </div>
+                        <div style={{ marginBottom: 100 }}></div>
+                    </Case>
                     <Case key="groups">Groups</Case>
                     <Case key="roadmap">Roadmap</Case>
                     <Case key="tasks">Tasks</Case>
