987444add5933d900b7d30f4a66041ca2c6c10fe
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-07-02 13:28:52 +0530

    fix(web): fix project home page content sizing

--- web/pages/project/[id]/home/index.tsx
+++ web/pages/project/[id]/home/index.tsx
@@ -6,37 +6,34 @@ import {
   FlagIcon,
   HornIcon,
   PersonIcon,
-  UseableIcon,
   GotoIcon
 } from "~/components/Icons";
 
 const WelcomeWidget: React.FC<{
   color: string;
-  icon: typeof UseableIcon;
+  Icon: any;
   title: string;
   description: string;
   contentClassName?: string;
-}> = ({ color, icon, title, description, contentClassName }) => {
-  const iconHolder = { icon };
-
+}> = ({ color, Icon, title, description, contentClassName }) => {
   return (
-    <div className="flex items-center justify-between h-28 w-120 bg-white-selected rounded-20 m-2 cursor-pointer select-none">
+    <div className="flex items-center justify-between h-28 max-w-xl bg-white-selected rounded-10 m-2 cursor-pointer select-none">
       <div className="flex items-center">
         <div
-          className="grid place-items-center rounded-full h-16 w-16 mx-5"
+          className="grid place-items-center rounded-full h-14 w-14 mx-3"
           style={{ backgroundColor: color }}
         >
-          {<iconHolder.icon />}
+          <div className="h-7 w-7">{Icon}</div>
         </div>
         <div
-          className={`flex flex-col justify-between items-start box-border h-20 py-1 ${contentClassName}`}
+          className={`flex flex-col justify-between items-start box-border py-1 w-96 ${contentClassName}`}
         >
-          <div className="font-bold text-2xl">{title}</div>
-          <div className="font-semibold text-base leading-5">{description}</div>
+          <div className="font-semibold text-xl">{title}</div>
+          <div className="text-base leading-5">{description}</div>
         </div>
       </div>
       <div className="flex justify-center items-center w-12 h-12 mr-10 rounded-10 hover:bg-white-hover-select">
-        <div className="w-10 h-10">
+        <div className="w-7 h-7">
           <GotoIcon />
         </div>
       </div>
@@ -54,37 +51,35 @@ const HomePage = () => {
         keywords="open source,hydralite,project management"
       />
       <ProjectLayout selected={"home"} centerContent={true}>
-          <div>
-            <h2 className="text-4xl font-black mb-3 text-center">
-              Welcome to <span className="text-green-700">Volt</span>
-            </h2>
-            <h3 className="text-xl font-semibold text mb-7 text-center">
-              Welcome to your shiny new project. Here’s some tips to get you
-              setup.
-            </h3>
+        <div>
+          <h2 className="text-4xl font-black mb-3 text-center">
+            Welcome to <span className="text-green-700">Volt</span>
+          </h2>
+          <h3 className="text-xl font-semibold text mb-7 text-center">
+            Welcome to your shiny new project. Here’s some tips to get you
+            setup.
+          </h3>
 
-            <WelcomeWidget
-              color="#FFA2D4"
-              icon={PersonIcon}
-              title="Invite your team"
-              description="Get your teammates on board, and let the collaboration begin."
-              contentClassName="w-96"
-            />
-            <WelcomeWidget
-              color="#A2B1FF"
-              icon={HornIcon}
-              title="Publicize your project"
-              description="Let others discover your project through hydra. Onboard potential contributors, all through one seamless platform."
-              contentClassName="w-112"
-            />
-            <WelcomeWidget
-              color="#FFA1A1"
-              icon={FlagIcon}
-              title="Start developing"
-              description="Go ahead and start building your project roadmap, View project traffic, and more"
-              contentClassName="w-96"
-            />
-          </div>
+          <WelcomeWidget
+            color="#FFA2D4"
+            Icon={<PersonIcon />}
+            title="Invite your team"
+            description="Get your teammates on board, and let the collaboration begin."
+          />
+          <WelcomeWidget
+            color="#FFA1A1"
+            Icon={<FlagIcon />}
+            title="Start developing"
+            description="Go ahead and start building your project roadmap, View project traffic, and more"
+          />
+
+          <WelcomeWidget
+            color="#A2B1FF"
+            Icon={<HornIcon />}
+            title="Publicize your project"
+            description="Onboard potential contributors and sponsors through hydra."
+          />
+        </div>
       </ProjectLayout>
     </>
   );
