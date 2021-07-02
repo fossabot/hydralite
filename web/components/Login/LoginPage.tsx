6a28ad9edf030c5a922f2032673a808c6d2f13c5
Shadow7258 <pranav.rdoshi@gmail.com>
2021-07-02 18:35:23 +0530

    fix(web) - added scss files for button and login page

--- web/components/Login/LoginPage.tsx
+++ web/components/Login/LoginPage.tsx
@@ -1,36 +1,60 @@
-import Button from '../Button/Button'
-import Meta from 'partials/Meta'
+import Button from "../Button/Button";
+import Meta from "partials/Meta";
+import styles from "./LoginPage.module.scss";
 
 const Login = () => {
-    return (
-        <>
-            <Meta
-                title="Hydralite"
-                description="Hydralite is the new open source platform for project management and open source project discovery."
-                url="https://hydralite.io"
-                keywords="open source,hydralite,project management"
-            />
+  return (
+    <>
+      <Meta
+        title="Hydralite"
+        description="Hydralite is the new open source platform for project management and open source project discovery."
+        url="https://hydralite.io"
+        keywords="open source,hydralite,project management"
+      />
 
-            <img src="/logo.png" alt="Logo" className="mt-4 ml-4 w-20" />
-            <div className="flex justify-center items-center">
-                <div className={styles.login_box}>
-                    
-                    <h1 className={styles.login_header}>Sign in to Hydralite</h1>
-                    <p className={styles.p_margin}>
-                        Seamless Project Management is just one click away.
-                    </p>
-                    <div className={styles.button_center}>
-                        <div>
-                            <Button className={styles.button_link} color="muted" text="Continue With Google" iconName="google" href="#"/>
-                            <Button className={styles.button_link} color="muted" text="Continue With GitHub" iconName="github" href="#"/>
-                            <Button className={styles.button_link} color="muted" text="Continue With Discord" iconName="discord" href="#"/>
-                            <Button className={styles.button_link} color="muted" text="Continue With Twitter" iconName="twitter" href="#"/>
-                        </div>
-                    </div>
-                </div>
+      <img src="/logo.png" alt="Logo" className="mt-4 ml-4 w-20" />
+      <div className="flex justify-center items-center">
+        <div className={styles.login_box}>
+          <h1 className={styles.login_header}>Sign in to Hydralite</h1>
+          <p className={styles.p_margin}>
+            Seamless Project Management is just one click away.
+          </p>
+          <div className={styles.button_center}>
+            <div>
+              <Button
+                className={styles.button_link}
+                color="muted"
+                text="Continue With Google"
+                iconName="google"
+                href="#"
+              />
+              <Button
+                className={styles.button_link}
+                color="muted"
+                text="Continue With GitHub"
+                iconName="github"
+                href="#"
+              />
+              <Button
+                className={styles.button_link}
+                color="muted"
+                text="Continue With Discord"
+                iconName="discord"
+                href="#"
+              />
+              <Button
+                className={styles.button_link}
+                color="muted"
+                text="Continue With Twitter"
+                iconName="twitter"
+                href="#"
+              />
             </div>
-        </>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
