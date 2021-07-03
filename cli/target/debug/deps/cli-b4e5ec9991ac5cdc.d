bc57933b509b4ba6c5f607ed0ca522ca06084426
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-07-02 23:54:06 -0700

    chore(cli): delete cargo.lock stuff

--- cli/target/debug/deps/cli-b4e5ec9991ac5cdc.d
+++
@@ -1,5 +0,0 @@
-/Users/adminaccount/Desktop/hydralite/cli/target/debug/deps/cli-b4e5ec9991ac5cdc.rmeta: src/main.rs
-
-/Users/adminaccount/Desktop/hydralite/cli/target/debug/deps/cli-b4e5ec9991ac5cdc.d: src/main.rs
-
-src/main.rs:
--- cli/target/debug/deps/cli-c920bf55247b23f0.d
+++
@@ -1,5 +0,0 @@
-/Users/adminaccount/Desktop/hydralite/cli/target/debug/deps/cli-c920bf55247b23f0.rmeta: src/main.rs
-
-/Users/adminaccount/Desktop/hydralite/cli/target/debug/deps/cli-c920bf55247b23f0.d: src/main.rs
-
-src/main.rs:
