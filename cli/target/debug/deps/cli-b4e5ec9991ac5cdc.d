4b8a40ebeef880c5b112f61f701527de0698bde1
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-07-02 23:31:31 -0700

    chore(api): remove joinDate in prisma schema as createdAt exists

---
+++ cli/target/debug/deps/cli-b4e5ec9991ac5cdc.d
@@ -0,0 +1,5 @@
+/Users/adminaccount/Desktop/hydralite/cli/target/debug/deps/cli-b4e5ec9991ac5cdc.rmeta: src/main.rs
+
+/Users/adminaccount/Desktop/hydralite/cli/target/debug/deps/cli-b4e5ec9991ac5cdc.d: src/main.rs
+
+src/main.rs:
---
+++ cli/target/debug/deps/cli-c920bf55247b23f0.d
@@ -0,0 +1,5 @@
+/Users/adminaccount/Desktop/hydralite/cli/target/debug/deps/cli-c920bf55247b23f0.rmeta: src/main.rs
+
+/Users/adminaccount/Desktop/hydralite/cli/target/debug/deps/cli-c920bf55247b23f0.d: src/main.rs
+
+src/main.rs:
