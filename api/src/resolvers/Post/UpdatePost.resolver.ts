a5e3eb83f9ebcaaa886c3751af2b2058d767504a
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-07-07 20:53:05 +0530

    feat(api): UpdatePost Resolver and Args

---
+++ api/src/resolvers/Post/UpdatePost.resolver.ts
@@ -0,0 +1,52 @@
+import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from "type-graphql";
+import { isAuthenticated } from "~/middleware/isAuthenticated.middleware";
+import ContextType from "~/types/Context.type";
+import { Post, User } from "~/resolver-types/models";
+import { connectIdArray } from "~/util/connectIdArray";
+import { ProjectMemberRepo } from "~/db/ProjectMemberRepo";
+import { UpdatePostArgs } from "./args/UpdatePostArgs";
+
+const memberRepo = new ProjectMemberRepo();
+@Resolver()
+export class UpdatePostResolver {
+  @UseMiddleware(isAuthenticated)
+  @Mutation(() => Post)
+  async updatePost(
+    @Arg("args") args: UpdatePostArgs,
+    @Ctx() { req, prisma }: ContextType
+  ) {
+    // retrieve the currently logged in user
+    const user: User = req.user as User;
+    const loggedInMember = await memberRepo.findMemberByUserAndProjectId(
+      user.id,
+      args.projectId
+    );
+
+    type postType = Parameters<typeof prisma.post.update>[0]["data"];
+    const post: postType = {};
+
+    if (args.title) post.title = args.title;
+    if (args.description) post.description = args.description;
+    if (args.hashtagIds) post.hashtags = connectIdArray(args.hashtagIds);
+    if (args.type) post.type = args.type;
+    if (args.categoryIds) post.categories = connectIdArray(args.categoryIds);
+    if (args.labelIds) post.labels = connectIdArray(args.labelIds);
+
+    // make sure user can manage posts in this project if they are trying to make this post an announcement or make a post private
+    if (args.isAnnouncement || !args.isPublic || args.visibleToRolesIds) {
+      memberRepo.memberHasPermission(loggedInMember!, "canModeratePosts");
+
+      post.isPublic = args.isPublic || false;
+      post.visibleTo = post.isPublic
+        ? connectIdArray(args.visibleToRolesIds)
+        : {};
+      post.isAnnouncement = args.isAnnouncement || false;
+    }
+
+    const updatedPost = await prisma.post.update({
+      where: { id: args.id },
+      data: post,
+    });
+    return updatedPost;
+  }
+}
---
+++ api/src/resolvers/Post/args/UpdatePostArgs.ts
@@ -0,0 +1,68 @@
+import { Field, InputType } from "type-graphql";
+import { PostType } from "~/resolver-types/enums";
+
+@InputType()
+export class UpdatePostArgs {
+  @Field((_type) => String, {
+    nullable: false,
+  })
+  id!: string;
+
+  @Field((_type) => String, {
+    nullable: true,
+  })
+  title?: string;
+
+  @Field((_type) => String, {
+    nullable: true,
+  })
+  description?: string;
+
+  @Field((_type) => String, {
+    nullable: false,
+  })
+  projectId!: string; // can not be used to alter the project a post lies in, only used to query post creator
+
+  @Field((_type) => Boolean, {
+    nullable: true,
+  })
+  isAnnouncement?: boolean;
+
+  @Field((_type) => PostType, {
+    nullable: true,
+  })
+  type?:
+    | "post"
+    | "article"
+    | "question"
+    | "feedback"
+    | "suggestion"
+    | "appreciation";
+
+  @Field((_type) => Boolean, {
+    nullable: true,
+  })
+  isPublic?: boolean;
+
+  @Field((_type) => [String], {
+    nullable: true,
+  })
+  labelIds?: string[];
+
+  @Field((_type) => [String], {
+    nullable: true,
+  })
+  categoryIds?: string[];
+
+  @Field((_type) => [String], {
+    nullable: true,
+  })
+  visibleToRolesIds?: string[];
+
+  // TASK: Add attachments
+
+  @Field((_type) => [String], {
+    nullable: true,
+  })
+  hashtagIds?: string[];
+}
