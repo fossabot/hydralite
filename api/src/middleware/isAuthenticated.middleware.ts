4f3828e526d636af490a0df39c7ea9f61bf896bc
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-07-07 16:51:46 +0530

    refactor(api): change deserializing logic

--- api/src/middleware/isAuthenticated.middleware.ts
+++ api/src/middleware/isAuthenticated.middleware.ts
@@ -3,9 +3,33 @@ import { MiddlewareFn } from "type-graphql";
 import ContextType from "~/types/Context.type";
 
 export const isAuthenticated: MiddlewareFn<ContextType> = async (
-  { context: { req } },
+  { context: { req, prisma } },
   next
 ) => {
   if (!req.isAuthenticated()) throw new ApolloError("Not Authenticated.");
+
+  const userId = (req.user as any).id;
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    include: {
+      ownedProjects: true,
+      allProjects: true,
+      likedProjects: true,
+      followedProjects: true,
+      followers: true,
+      following: true,
+      oauthConnections: true,
+      profile: true,
+      createdHashtags: true,
+      createdPostComments: true,
+      posts: true,
+      bugReports: true,
+      featureRequests: true,
+    },
+  });
+  req.user = user as any;
+
   return next();
 };
