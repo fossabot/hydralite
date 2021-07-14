744b7d3ea56346621d3d8e4d872c4d7cf5cdcae0
Amitoj Singh <amitojsingh366@gmail.com>
2021-07-14 16:08:20 +0300

    refactor(hydrabot): refactor bot

---
+++ hydrabot/src/index.ts
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+import { Bot } from './classes/bot';
+const client = new Bot();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+    client.registerCommands(process.env.GUILD_ID || "");
+});
+
+
+client.interactions.on("commandInteraction", (interaction) => {
+    console.log(client.commands);
+
+    if (client.commands) {
+        if (!client.commands.has(interaction.name))
+            return interaction.respond({ content: "Couldn't find command!", isPrivate: true })
+        client.commands.get(interaction.name)?.execute(client, interaction);
+    }
+})
+
+client.login(process.env.TOKEN);
