218e60d8c5075299fccfad86581217c21f958cd9
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-07-16 17:52:22 +0100

    chore(api): change to more efficient imports

--- api/src/resolvers/Hashtag/FindHashtagById.resolver.ts
+++ api/src/resolvers/Hashtag/FindHashtagById.resolver.ts
@@ -2,7 +2,7 @@ import { Arg, Ctx, Query, Resolver, UseMiddleware } from "type-graphql";
 import ContextType from "~/types/Context.type";
 import executeOrFail from "~/util/executeOrFail";
 import { isAuthenticated } from "~/middleware/isAuthenticated.middleware";
-import { Hashtag } from "src/resolver-types";
+import { Hashtag } from "src/resolver-types/models/Hashtag";
 import { FindHashtagByIdArgs } from "./args/FindHashtagByIdArgs";
 
 @Resolver()
