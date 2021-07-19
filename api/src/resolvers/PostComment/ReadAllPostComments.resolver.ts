318ab0f48d37ac23211af4696982df1dc654df57
firecraftgaming <eliyah@sundstroem.com>
2021-07-19 14:24:03 +0200

    feat(api): added resolver readallpostcomments

---
+++ api/src/resolvers/PostComment/ReadAllPostComments.resolver.ts
@@ -0,0 +1,21 @@
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+import { IsAuthenticated } from "~/middleware/isAuthenticated.middleware";
+import { ReadAllPostCommentsArgs } from "./args/ReadAllPostCommentsArgs";
+import ContextType from "~/types/Context.type";
+import { PostComment } from "~/resolver-types/models";
+
+@Resolver()
+export class ReadAllPostCommentsResolver {
+  @Query(() => [PostComment])
+  @IsAuthenticated()
+  async readAllPostComments(
+    @Arg("args") args: ReadAllPostCommentsArgs,
+    @Ctx() { req, prisma }: ContextType
+  ) {
+    return await prisma.postComment.findMany({
+      where: {
+        postId: args.postId
+      }
+    });
+  }
+}
