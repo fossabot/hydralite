61af098ae1696004cb3fa3292e6b87ec3135a913
firecraftgaming <eliyah@sundstroem.com>
2021-07-19 09:48:10 +0200

    fix(api): hashtag used generated tql instead of resolver types

--- api/src/resolvers/Hashtag/FindHashtagByName.resolver.ts
+++ api/src/resolvers/Hashtag/FindHashtagByName.resolver.ts
@@ -2,7 +2,7 @@ import { Arg, Ctx, Query, Resolver, UseMiddleware } from "type-graphql";
 import ContextType from "~/types/Context.type";
 import executeOrFail from "~/util/executeOrFail";
 import { isAuthenticated } from "~/middleware/isAuthenticated.middleware";
-import { Hashtag } from "src/typegql-generated";
+import { Hashtag } from "src/resolver-types";
 import { FindHashtagByNameArgs } from "./args/FindHashtagByNameArgs";
 
 @Resolver()
