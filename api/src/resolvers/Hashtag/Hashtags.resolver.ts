fed45fa67ca8d45bddc06e0fa686d27247d27dca
firecraftgaming <eliyah@sundstroem.com>
2021-07-20 23:46:10 +0200

    fix(api): fixed issue with hashtag searching in src/ then ~/

--- api/src/resolvers/Hashtag/Hashtags.resolver.ts
+++ api/src/resolvers/Hashtag/Hashtags.resolver.ts
@@ -2,7 +2,7 @@ import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
 import ContextType from "~/types/Context.type";
 import executeOrFail from "~/util/executeOrFail";
 import { IsAuthenticated } from "~/middleware/isAuthenticated.middleware";
-import { Hashtag } from "src/resolver-types/models/Hashtag";
+import { Hashtag } from "~/resolver-types/models/Hashtag";
 
 @Resolver()
 export default class HashtagsResolver {
