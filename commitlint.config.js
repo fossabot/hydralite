dca8b5177a034428d0dad6e8beb8fb9ac65c3145
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-07-23 11:52:17 +0000

    chore(commitlint): add more scopes + comments for documentation/usage
    I commented in this file A LOT. Includes new scoped including prettier (for ignore file used by
    Prettier and configuration), meta (code of conduct and other meta stuff you might think),
    github-actions/actions/gh-actions (GitHub Actions stuff) and gitlab-ci (in case Hydralite project
    itself just switched to GitLab), among other stuff.
    
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,17 +5,26 @@ module.exports = {
       2,
       "always",
       [
-        "global",
-        "api",
-        "landing",
-        "automation",
-        "mobile",
-        "prototype",
-        "web",
-        "hydrabot",
-        "docker",
-        "gitpod",
-        "devcontainer",
+        "global", // project-wide stuff, README included
+        "api", // backend
+        "landing", // landing page
+        "automation", // automation scripts
+        "mobile", // Flutter mobile app
+        "prototype", // prototypes
+        "web", // frontend
+        "hydrabot", // Discord bot/GitHub App
+        "docker", // Docker Compose, Docckerfiles
+        "gitpod", // Gitpod
+        "devcontainer", // devcontainer dir
+        "docs", // just in case you didn't use docs type
+        "contributing-guide", // CONTRIBUTING.md
+        "meta", // code of conduct
+        "commitlint", // this file OwO
+        "issue-templates", // Issue templates
+        "prettier", // prettierrc, prettierignore
+        "assets", // images, logos, screenshots
+        "github-actions", "actions", "gh-actions", // GitHub Actions
+        "gitlab-ci" // GitLab CI/CD 
       ],
     ],
   },
