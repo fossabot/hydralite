c032d8fa838216afd63856d5d3d086b79536a07b
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-07-26 22:53:38 +0530

    chore(web): remove tailwind style extensions

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -17,37 +17,7 @@ module.exports = {
           yellow: "#EFA119",
         },
       },
-    },
-    extend: {
-      borderRadius: {
-        5: "5px",
-        8: "8px",
-        10: "10px",
-        20: "20px",
-        40: "40px",
-        half: "50%",
-      },
-      colors: {
-        bluegrey: "#9099a8",
-      },
-      spacing: {
-        "1px": "1px",
-        "2px": "2px",
-        4.5: "1.1rem",
-        7.5: "30px",
-        "28rem": "28rem",
-        "30rem": "30rem",
-        "35rem": "35rem",
-        "40rem": "40rem",
-        "45rem": "45rem",
-        "50rem": "50rem",
-        "55rem": "55rem",
-        "9vh": "9vh",
-      },
-      fontSize: {
-        md: "0.9rem",
-      },
-    },
+    }
   },
   variants: {},
   plugins: [],
