a214b848ae52d61338427841ea87a1290deeb59d
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-07-26 11:51:44 +0000

    feat(commitlint): customize Conventional Changelog's types to include merge type
    Also added an documentation for usage of these types and scopes.
    
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,17 @@
+/* We want to customize the defaults in @commitlint/config-conventional, so
+ * our Commitlint CI doesn't complain about "merge(pr|git-pull): ..." in the future
+ */
+const conventionalCommit = require('./cz-commitlint-config/custom-types.json');
+const typesEnum = Object.keys(conventionalCommit.types);
+
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "type-enum": [
+        2,
+        "always",
+        typesEnum
+    ],
     "scope-enum": [
       2,
       "always",
@@ -16,17 +27,20 @@ module.exports = {
         "docker", // Docker Compose, Docckerfiles
         "gitpod", // Gitpod
         "devcontainer", // devcontainer dir
-        "docs", // just in case you didn't use docs type
+        "docs", // just in case you didn't use docs type. if used the docs type, DO NOT USE THIS SCOPE
         "contributing-guide", // CONTRIBUTING.md
-        "meta", // code of conduct
-        "commitlint", // this file OwO
+        "meta", // code of conduct, other sort of meta stuff. sometimes, README and CONTRIBUTING.md may fall into this in some cases
+        "commitlint", // Commitlint stuff, especially this file OwO
+        "commitizen", // Commitizen stuff
         "issue-templates", // Issue templates
-        "prettier", // prettierrc, prettierignore
+        "prettier", // prettierr, prettierignore
         "assets", // images, logos, screenshots
-        "github-actions", "actions", "gh-actions", // GitHub Actions
+        "github-a tions", "actions", "gh-actions", // GitHub Actions
         "gitlab-ci", // GitLab CI/CD
         "deps-dev", "deps-peer", "deps-optional", "deps", // Dependabot stuff
-        "lockfiles" // yarn.lock and Cargo.lock
+        "lockfiles", // yarn.loommitik and Cargo.lock
+        "pr", "mr", "pull-request", "merge-request", // GitHub/GitLab stuff for pull requests
+        "git-pull" // the legendary 'git pull upstream dev'
       ],
     ],
   },
