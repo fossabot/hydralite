bc3baafd841a9162d6e989faf07d3a20ae422d72
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-07-27 20:00:14 +0530

    feat(web): add active state icons to sidebar

--- web/components/Sidebar/Sidebar.tsx
+++ web/components/Sidebar/Sidebar.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { useThemeContext } from "~/hoc/theme/ThemeContext";
 import SidebarTab from "./SidebarTab";
 import HistoryIcon from "~/icons/line/HistoryIcon"
+import HistoryIconSolid from "~/icons/solid/HistoryIconSolid"
 import ListUlIcon from "~/icons/line/ListUlIcon"
+import ListUlIconSolid from "~/icons/solid/ListUlIconSolid"
 import CompassIcon from "~/icons/line/CompassIcon"
+import CompassIconSolid from "~/icons/solid/CompassIconSolid"
 import MessagesIcon from "~/icons/line/MessagesIcon"
+import MessagesIconSolid from "~/icons/solid/MessagesIconSolid"
 
 const Sidebar = ({ projects }: { projects: {}[] }) => {
   const { theme } = useThemeContext();
@@ -18,31 +22,35 @@ const Sidebar = ({ projects }: { projects: {}[] }) => {
     >
       <SidebarTab
         isActive={false}
-        activeIcon={<img src="/HydraliteLogoMonochrome.svg" className="select-none h-[2.7rem] w-[2.7rem]" draggable={false} />}
+        activeIcon={<img src="/HydraliteLogoMonochrome.svg" className="select-none h-[2.2rem] w-[2.2rem]" draggable={false} />}
         icon={<img src="/HydraliteLogo.svg" className="select-none h-[2.2rem] w-[2.2rem]" draggable={false} />}
         name="Home"
         link="/"
       />
       <SidebarTab
         isActive={false}
+        activeIcon={<HistoryIconSolid className="h-[1.9rem] w-[1.9rem] fill-[#fff]" />}
         icon={<HistoryIcon className="h-[1.9rem] w-[1.9rem] fill-[#fff]" />}
         name="Home"
         link="/"
       />
       <SidebarTab
         isActive={false}
+        activeIcon={<ListUlIconSolid className="h-[1.9rem] w-[1.9rem] fill-[#fff]" />}
         icon={<ListUlIcon className="h-[1.9rem] w-[1.9rem] fill-[#fff]" />}
         name="Home"
         link="/"
       />
       <SidebarTab
         isActive={false}
+        activeIcon={<CompassIconSolid className="h-[1.9rem] w-[1.9rem] fill-[#fff]" />}
         icon={<CompassIcon className="h-[1.9rem] w-[1.9rem] fill-[#fff]" />}
         name="Home"
         link="/"
       />
       <SidebarTab
         isActive={false}
+        activeIcon={<MessagesIconSolid className="h-[1.9rem] w-[1.9rem] fill-[#fff]" />}
         icon={<MessagesIcon className="h-[1.9rem] w-[1.9rem] fill-[#fff]" />}
         name="Home"
         link="/"
