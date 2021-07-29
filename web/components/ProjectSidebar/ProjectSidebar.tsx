04cf200e7ef11eaa51b641d8da6b195934141eb8
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-07-29 14:50:59 +0530

    feat(web): seperate tab sections for project sidebar

--- web/components/ProjectSidebar/ProjectSidebar.tsx
+++ web/components/ProjectSidebar/ProjectSidebar.tsx
@@ -13,19 +13,27 @@ const ProjectSidebar = () => {
       }`}
     >
       <div
-        className="flex flex-col w-full py-2 pl-4"
+        className="flex flex-col w-full py-2 pl-4 h-[3.5rem]"
         style={{ boxShadow: "0px 4px 4px 0px #1F1F1F40" }}
       >
         <h1 className="font-bold text-md">Project Hydralite</h1>
         <h3 className="font-medium text-xs">420 members</h3>
       </div>
-      <div className="flex flex-col gap-5 p-3 pl-4 mt-2">
-        <ProjectSidebarTab
-          name="Home"
-          Icon={
-            HomeAltIcon
-          }
-        />
+      <div className="flex flex-col p-3 pl-4 mt-2 h-[calc(100%-3.5rem)]">
+        <div className="flex flex-col gap-5 mb-auto">
+          <ProjectSidebarTab name="Home" Icon={HomeAltIcon} />
+          <ProjectSidebarTab name="Home" Icon={HomeAltIcon} />
+          <ProjectSidebarTab name="Home" Icon={HomeAltIcon} />
+          <ProjectSidebarTab name="Home" Icon={HomeAltIcon} />
+          <ProjectSidebarTab name="Home" Icon={HomeAltIcon} />
+          <ProjectSidebarTab name="Home" Icon={HomeAltIcon} />
+          <ProjectSidebarTab name="Home" Icon={HomeAltIcon} />
+          <ProjectSidebarTab name="Home" Icon={HomeAltIcon} />
+        </div>
+        <div className="flex flex-col gap-5 mt-auto">
+          <ProjectSidebarTab name="Home" Icon={HomeAltIcon} />
+          <ProjectSidebarTab name="Home" Icon={HomeAltIcon} />
+        </div>
       </div>
     </div>
   );
