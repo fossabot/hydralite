370931e0d80e6d1afb8fbc5c925c619c8d11de5a
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-08-01 14:06:29 +0530

    feat(api): add createInterest mutation

--- api/src/resolvers/Interest/CreateInterest.resolver.ts
+++ api/src/resolvers/Interest/CreateInterest.resolver.ts
@@ -1,7 +1,30 @@
-// import { Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { IsAuthenticated } from "~/middleware/isAuthenticated.middleware";
+import { Interest, User } from "~/resolver-types/models";
+import ContextType from "~/types/Context.type";
+import executeOrFail from "~/util/executeOrFail";
+import { CreateInterestArgs } from "./args/CreateInterestArgs";
 
-// @Resolver()
-// class CreateInterestResolver {
-//     @Mutation(() => Interest)
+@Resolver()
+export class CreateInterestResolver {
+  @Mutation(() => Interest)
+  @IsAuthenticated()
+  createInterest(
+    @Arg("args") { name }: CreateInterestArgs,
+    @Ctx() { req, prisma }: ContextType
+  ): Promise<Interest | null> {
+    // retrieve the currently logged in user
+    const user: User = req.user as User;
 
-// }
+    return executeOrFail(async () => {
+      const createdInterest = await prisma.interest.create({
+        data: {
+          name,
+          creator: { connect: { id: user.id } },
+          uses: 0,
+        },
+      });
+      return createdInterest;
+    });
+  }
+}
