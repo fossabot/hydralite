82a89c7a24a5333bf80b7e9fc7f28ab2bb434466
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-08-03 18:02:03 +0530

    feat(api): add CreateOpportunityArgs

--- api/src/resolvers/Opportunity/args/CreateOpportunityArgs.ts
+++ api/src/resolvers/Opportunity/args/CreateOpportunityArgs.ts
@@ -1,13 +1,37 @@
-// import { Field, InputType } from "type-graphql";
+import { Field, InputType } from "type-graphql";
 
-// @InputType()
-// export class CreateOpportunityArgs {
-//     @Field()
-//     projectId: string;
+type TimeFrequency = "hourly" | "daily" | "weekly" | "monthly" | "yearly";
+@InputType()
+export class CreateOpportunityArgs {
+  @Field()
+  projectId: string;
 
-//     @Field()
-//     title: string;
+  @Field()
+  title: string;
 
-//     @Field()
-//     description: string
-// }
+  @Field()
+  description: string;
+
+  @Field(() => [String])
+  requiredSkillIds: String[];
+
+  @Field({ nullable: true })
+  isPaid?: string;
+
+  // TASK: Figure out payment amount stuff
+
+  @Field({
+    nullable: true,
+    description: "Frequency of the capacity and payment",
+  })
+  // NOTE: Only applicable if opportunity is paid
+  timeFrequency?: TimeFrequency;
+
+  @Field({
+    nullable: true,
+    description:
+      "Hours a member must dedicate to this project based on a particular timeFrequency",
+  })
+  // NOTE: Only applicable if opportunity is paid
+  requiredCapacity?: number;
+}
