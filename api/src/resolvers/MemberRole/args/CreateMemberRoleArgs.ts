7d8800b977962407c1fa577e8fd4f2a41544e26a
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-08-04 23:03:11 +0530

    fix(api): resolve dependency issue

--- api/src/resolvers/MemberRole/args/CreateMemberRoleArgs.ts
+++ api/src/resolvers/MemberRole/args/CreateMemberRoleArgs.ts
@@ -1,4 +1,5 @@
 import { Field, InputType } from "type-graphql";
+import { MemberPerms } from "~/types/MemberPerms.type";
 
 @InputType()
 export class CreateMemberRoleArgs {
@@ -11,66 +12,6 @@ export class CreateMemberRoleArgs {
   @Field()
   projectId: string;
 
-  @Field({ nullable: true })
-  canAccessFeatureRequests?: boolean;
-
-  @Field({ nullable: true })
-  canAccessBugReports?: boolean;
-
-  @Field({ nullable: true })
-  canManageUsers?: boolean;
-
-  @Field({ nullable: true })
-  canManageRoles?: boolean;
-
-  @Field({ nullable: true })
-  canManageTasks?: boolean;
-
-  @Field({ nullable: true })
-  canCreateProjectAnnouncements?: boolean;
-
-  @Field({ nullable: true })
-  canManagePosts?: boolean;
-
-  @Field({ nullable: true })
-  canManageProjectGroups?: boolean;
-
-  @Field({ nullable: true })
-  canGenerateProjectInvites?: boolean;
-
-  @Field({ nullable: true })
-  canViewRoadmap?: boolean;
-
-  @Field({ nullable: true })
-  canManageRoadmap?: boolean;
-
-  @Field({ nullable: true })
-  canScheduleRooms?: boolean;
-
-  @Field({ nullable: true })
-  canManageFundraisers?: boolean;
-
-  @Field({ nullable: true })
-  canManageThirdPartyApps?: boolean;
-
-  @Field({ nullable: true })
-  canManageProjectSurveys?: boolean;
-
-  @Field({ nullable: true })
-  canViewProjectInsights?: boolean;
-
-  @Field({ nullable: true })
-  canViewDeveloperInsights?: boolean;
-
-  @Field({ nullable: true })
-  canManageProjectWaitlists?: boolean;
-
-  @Field({ nullable: true })
-  canManageTesterOutsourcing?: boolean;
-
-  @Field({ nullable: true })
-  canWriteDeveloperReviews?: boolean;
-
-  @Field({ nullable: true })
-  canViewLogs?: boolean;
+  @Field(() => [String])
+  perms?: MemberPerms[];
 }
