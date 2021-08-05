e573b6688d0636e24fbdc197b0e59a01cbf4f0e8
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-08-05 22:32:11 +0530

    chore(api): change find opp. find resolvers from mutations to query

--- api/src/resolvers/Opportunity/FindOpportunityById.resolver.ts
+++ api/src/resolvers/Opportunity/FindOpportunityById.resolver.ts
@@ -1,11 +1,11 @@
-import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { Opportunity } from "~/resolver-types/models";
 import ContextType from "~/types/Context.type";
 import { FindOpportunityByIdArgs } from "./args/FindOpportunityByIdArgs";
 
 @Resolver()
 export class FindOpportunityByIdResolver {
-  @Mutation(() => Opportunity)
+  @Query(() => Opportunity)
   async findOpportunityById(
     @Arg("args") { opportunityId }: FindOpportunityByIdArgs,
     @Ctx() { prisma }: ContextType
