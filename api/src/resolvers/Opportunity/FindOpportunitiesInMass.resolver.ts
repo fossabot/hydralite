4b19298b5b9cc528b398d5056894ddb2d6a05780
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-08-05 20:41:15 +0530

    feat(api): paginate findOpportunitiesInMass mutation

--- api/src/resolvers/Opportunity/FindOpportunitiesInMass.resolver.ts
+++ api/src/resolvers/Opportunity/FindOpportunitiesInMass.resolver.ts
@@ -1,15 +1,19 @@
-import { Ctx, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
 import { Opportunity } from "~/resolver-types/models";
 import ContextType from "~/types/Context.type";
+import { FindOpportunitiesInMassArgs } from "./args/FindOpportunitiesInMassArgs";
 
 @Resolver()
 export class FindOpportunitiesInMassResolver {
   @Mutation(() => [Opportunity])
   async findOpportunitiesInMass(
+    @Arg("args") { limit, skip }: FindOpportunitiesInMassArgs,
     @Ctx() { prisma }: ContextType
   ): Promise<Opportunity[] | null> {
     // TASK: personalize based on users interests
-    // TASK: add pagination
-    return await prisma.opportunity.findMany({});
+    return await prisma.opportunity.findMany({
+      skip,
+      take: limit,
+    });
   }
 }
