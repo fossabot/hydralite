95acea0a7c071e62216fb3af9f296082741cfb1c
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-08-05 16:00:12 +0530

    feat(api): add updateOpportunity mutation

---
+++ api/src/resolvers/Opportunity/UpdateOpportunity.resolver.ts
@@ -0,0 +1,74 @@
+import { User } from "@prisma/client";
+import { ApolloError } from "apollo-server-express";
+import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { ProjectMemberRepo } from "~/db/ProjectMemberRepo";
+import { IsAuthenticated } from "~/middleware/isAuthenticated.middleware";
+import ContextType from "~/types/Context.type";
+import { connectIdArray } from "~/util/connectIdArray";
+import executeOrFail from "~/util/executeOrFail";
+import { UpdateOpportunityArgs } from "./args/UpdateOpportunityArgs";
+
+const memberRepo = new ProjectMemberRepo();
+
+@Resolver()
+export class UpdateOpportunityResolver {
+  @Mutation()
+  @IsAuthenticated()
+  async updateOpportunity(
+    @Arg("args") args: UpdateOpportunityArgs,
+    @Ctx() { prisma, req }: ContextType
+  ) {
+    // retrieve the currently logged in user
+    const user: User = req.user as User;
+
+    /* validation */
+
+    // find the opportunity to update
+    const opportunity = await prisma.opportunity.findUnique({
+      where: { id: args.opportunityId },
+    });
+    if (!opportunity) throw new ApolloError("Could not find opportunity");
+
+    // ensure this user exists in the project of the opportunity
+    const loggedInMember = await memberRepo.findMemberByUserAndProjectId(
+      user.id,
+      opportunity.projectId
+    );
+
+    // ensure this user has perms in the specified project
+    memberRepo.memberHasPermission(
+      loggedInMember!,
+      "canManageContributorOutsourcing"
+    );
+
+    type opportunityType = Parameters<
+      typeof prisma.opportunity.update
+    >[0]["data"];
+    const opportunityData: opportunityType = {
+      title: args.title,
+      description: args.description,
+      isPaid: !!args.isPaid,
+      requiredSkills: connectIdArray(args.requiredSkillIds),
+      paymentAmount: null, // TASK: change once payment stuff is figured out
+      requiredCapacity: null,
+      timeFrequency: null,
+    };
+
+    // conditionally set null fields
+    if (args.isPaid) {
+      opportunityData.timeFrequency = args.timeFrequency;
+      opportunityData.requiredCapacity = args.requiredCapacity;
+      // TASK: add payment amount once payment stuff is figured out
+    }
+
+    const updatedOpportunity = await executeOrFail(
+      async () =>
+        await prisma.opportunity.update({
+          data: opportunityData,
+          where: { id: opportunity.id },
+        })
+    );
+
+    return updatedOpportunity;
+  }
+}
