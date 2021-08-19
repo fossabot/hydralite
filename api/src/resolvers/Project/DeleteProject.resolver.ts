a2aa454cf97a2ae01a4a74cd9f47716fa86e69da
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-08-19 13:06:34 +0100

    refactor(api): add more validation to delete project resolver
    Signed-off-by: Bedanta Mukhopadhay <ongshu777@gmail.com>

--- api/src/resolvers/Project/DeleteProject.resolver.ts
+++ api/src/resolvers/Project/DeleteProject.resolver.ts
@@ -1,17 +1,30 @@
+import { User } from "@prisma/client";
+import { ApolloError } from "apollo-server-express";
 import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { IsAuthenticated } from "~/middleware/isAuthenticated.middleware";
 import ContextType from "~/types/Context.type";
 import { DeleteProjectArgs } from "./args/DeleteProjectArgs";
 
 @Resolver()
 export class DeleteProjectResolver {
   @Mutation(() => Boolean)
+  @IsAuthenticated()
   async deleteProject(
     @Arg("args") { projectId }: DeleteProjectArgs,
-    @Ctx() { prisma }: ContextType
+    @Ctx() { prisma, req }: ContextType
   ) {
-    await prisma.project.delete({
+    const user = req.user as User;
+
+    const project = await prisma.project.findUnique({
       where: { id: projectId },
     });
+
+    if (!project) throw new ApolloError("Project does not exist");
+
+    if (project.ownerId !== user.id)
+      throw new ApolloError("You are not authorized to delete this project");
+
+    await prisma.project.delete({ where: { id: projectId } });
 
     return true;
   }
