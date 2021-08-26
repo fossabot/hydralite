b0a1033318f6d7e4e0b2b7540ed934925183571e
Lint Action <lint-action@samuelmeuli.com>
2021-08-26 14:12:19 +0000

    chore(lint): fix code style issues

--- web/components/CreateRippleModal/CreateRippleModal.tsx
+++ web/components/CreateRippleModal/CreateRippleModal.tsx
@@ -3,14 +3,18 @@ import { Modal } from "../Modal/Modal";
 import { CreateRippleModalBody } from "./CreateRippleModalBody";
 import { CreateRippleModalHeader } from "./CreateRippleModalHeader";
 
-interface ICreateRippleModal{
-  isOpen: any,
-  setIsOpen: any,
+interface ICreateRippleModal {
+  isOpen: any;
+  setIsOpen: any;
 }
 export const CreateRippleModal = (props: ICreateRippleModal) => {
   return (
     <>
-      <Modal isOpen={props.isOpen} setIsOpen={props.setIsOpen} width="w-[35rem]">
+      <Modal
+        isOpen={props.isOpen}
+        setIsOpen={props.setIsOpen}
+        width="w-[35rem]"
+      >
         <CreateRippleModalHeader
           projectName="hydralite"
           closeModal={() => props.setIsOpen(true)}
