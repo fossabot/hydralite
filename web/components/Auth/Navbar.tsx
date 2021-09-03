faf578e3a5c3f68fe15a4faa58c1b78f228e4480
VarunPotti <potti.varun07@gmail.com>
2021-09-03 13:23:54 +0530

    fix(web): Spacing

--- web/components/Auth/Navbar.tsx
+++ web/components/Auth/Navbar.tsx
@@ -2,18 +2,18 @@ import Image from "next/image"
 import ArrowIconDown from "../../icons/solid/ArrowIconDown"
 export default function Navbar() {
   return (
-    <div className="w-[98%] h-16 font-montserrat flex ml-4 items-center justify-between gap-2 mx-2 select-none">
+    <div className="w-[100%] h-14 font-montserrat flex items-center justify-between gap-2 select-none mx-2">
       <h1 className="text-2xl tracking-wide font-extrabold md:text-lg">HYDRALITE</h1>
-      <div className="flex gap-3 font-semibold text-xl md:text-base ">
+      <div className="flex gap-8 font-semibold text-xl md:text-base ">
         <div>Discover</div>
         <div className="text-dark-color-accent">Projects</div>
         <div>Profile</div>
         <div>Blog</div>
         <div>Project Ideas</div>
       </div>
-      <div className="flex bg-iris-10 dark:bg-dark-bgMuted1 w-44 h-11 rounded items-center justify-center gap-3 px-2">
-        <Image src="https://avatars.githubusercontent.com/u/77481923?v=4" height="35%" width="35%" className="rounded-xl" ></Image>
-        <span className="text-lg">VarunPotti</span>
+      <div className="flex bg-iris-10 dark:bg-dark-bgMuted1 w-44 h-11 rounded-lg items-center justify-center gap-2">
+        <Image src="https://avatars.githubusercontent.com/u/77481923?v=4" height="25%" width="25%" className="rounded-xl" ></Image>
+        <span className="text-sm">VarunPotti</span>
         <div className=" dark:text-white text-gray-900">
           <ArrowIconDown className="fill-current" />
         </div>
--- web/components/Auth/Sidebar/Sidebar.tsx
+++ web/components/Auth/Sidebar/Sidebar.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 const SidebarItem = (props) => {
   return (
     <Link href={`/projects/${props.title}`}>
-      <div className="mb-3 md:ml-0 ml-5 md:h-10 md:w-10 lg:h-12 lg:w-12 relative has-tooltip">
+      <div className="mb-3 md:ml-0 ml-5 md:h-10 md:w-10 lg:h-12 lg:w-12 relative has-tooltip ">
         <Image
           src={props.img}
-          className="rounded_box duration-200 cursor-pointer"
+          className="duration-200 cursor-pointer rounded_box"
           alt=""
           width="50em"
           height="50em"
@@ -30,9 +30,9 @@ export default function Sidebar() {
         title="Dogehouse"
         img="https://cdn.discordapp.com/icons/810571477316403233/874696134fd345707cc17d29266a1d49.png?size=128"
       />
-      <div className="md:w-10 md:h-10 md:ml-0 lg:w-12 lg:h-12 ml-5 my-2 rounded_box flex items-center justify-center bg-iris-10 dark:bg-dark-bgMuted1 has-tooltip cursor-pointer">
+      <div className="md:w-10 md:h-10 md:ml-0 lg:w-12 lg:h-12 ml-5 my-2 flex items-center justify-center bg-iris-10 dark:bg-dark-bgMuted1 has-tooltip cursor-pointer rounded_box">
         <PlusIconSolid className="w-[60%] fill-current" />
-        <span className="tooltip rounded absolute w-auto py-2 p-1 pr-2 truncate shadow-xl bg-iris-10 dark:bg-dark-bgMuted2 text-black dark:text-white font-montserrat font-montserrat font-semiboldrabold ml-44">
+        <span className="tooltip absolute w-auto py-2 p-1 pr-2 truncate shadow-xl bg-iris-10 dark:bg-dark-bgMuted2 text-black dark:text-white font-montserrat ml-44">
           New Project
         </span>
       </div>
