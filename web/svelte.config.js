4fdbb4b9a99a6c3202b192c3eb5860956d77a1c4
VarunPotti <potti.varun07@gmail.com>
2021-09-13 17:46:26 +0530

    feat(web): added netlify.toml and set up svelte for netlify deployements

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from "svelte-preprocess";
-
+import netlify from "@sveltejs/adapter-netlify"
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -7,6 +7,8 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
+    adapter: netlify(),
+
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: "#svelte",
 	},
