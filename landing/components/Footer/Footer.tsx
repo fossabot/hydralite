4ebb1c9687bbd7db75ffe4e5e2eacaf5bfa9604f
Lint Action <lint-action@samuelmeuli.com>
2021-09-14 07:30:29 +0000

    chore(lint): fix code style issues

--- landing/components/Footer/Footer.tsx
+++ landing/components/Footer/Footer.tsx
@@ -4,7 +4,10 @@ import styles from "./Footer.module.css";
 const Footer = () => (
   <div className={styles.footer}>
     <img src="/logo.png" alt="" className={styles.footer__logo} />
-    <h3 className={styles.footer__credit}>Designed and Implemented by <a href="https://github.com/fullstackslayer">Aaryaman Maheshwari</a></h3>
+    <h3 className={styles.footer__credit}>
+      Designed and Implemented by{" "}
+      <a href="https://github.com/fullstackslayer">Aaryaman Maheshwari</a>
+    </h3>
   </div>
 );
 
